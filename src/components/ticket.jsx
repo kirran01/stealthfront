fce7e817315b0bb54bc772ef1657dfa5f68a122c
kirran01 <Kirran-@live.com>
2023-02-16 14:15:55 -0400

    ticket ui

--- src/components/ticket.jsx
+++ src/components/ticket.jsx
@@ -3,8 +3,17 @@ import axios from 'axios';
 import { useState } from 'react';
 const Ticket = () => {
     return (
-        <div>
-            tick
+        <div className='flex flex-col items-center rounded-md shadow-sm max-w-md bg-white'>
+            <div className='flex justify-between w-full'>
+                <p className='m-2 flex items-center'>status</p>
+                <button className='bg-slate-50 rounded-lg m-2 p-2'>delete</button>
+            </div>
+            <div className='flex justify-center items-center m-5'>
+                <p className='text-xl'>email</p>
+            </div>
+            <div className='bg-slate-50 rounded-md max-w-md m-5'>
+                <p>Intelligentsia tilde tumeric hot chicken freegan air plant subway tile typewriter bushwick tonx single-origin coffee scenester. Actually chia lumbersexual copper mug put a bird on it snackwave mustache franzen migas artisan poke roof party plaid gatekeep. Cliche ennui taxidermy put a bird on it.</p>
+            </div>
         </div>
     );
 }
--- src/pages/ticketpage.jsx
+++ src/pages/ticketpage.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import Ticket from '../components/ticket';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Ticketpage = () => {
+    const [tickets, setTickets] = useState([])
+    useEffect(() => {
+        const getTickets = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3000/tickets/get-tickets`, {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem('authToken')}`
+                    }
+                })
+                if (res) {
+                    console.log(res.data, 'rd')
+                    setTickets(res.data)
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getTickets()
+    }, [])
+
     return (
-        <div>
-            ticketpage
+        <div className='bg-cyan-50 flex flex-col items-center'>
+            <p className='text-center text-2xl'>Tickets</p>
+            <div>
+                <Ticket />
+            </div>
         </div>
     );
 }
